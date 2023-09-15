daca3afcc052316683314712356df5dc715b2269
Paul Onawola <onawola.ire@gmail.com>
2023-09-15 11:46:02 +0100

    Initial Commit

---
+++ Bucketlist/BucketlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketlistApp.swift
+//  Bucketlist
+//
+//  Created by Paul Onawola on 15/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct BucketlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
